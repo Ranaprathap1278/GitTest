0356f1b441bd1fc7ef50474ae01df4964ee14068
Ranaprathap1278 <sankineniranaprathap@gmail.com>
2023-04-03 17:56:17 +0530

    Initial Changes

---
+++ GitSession/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Rana's Section
+Console.WriteLine("My Name is Rana");
+
+// Raj's Section.
+
+
+//Sukrutha's Section
